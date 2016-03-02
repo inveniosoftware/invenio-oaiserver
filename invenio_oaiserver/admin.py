b1665ff21d8aec24b3468d9016919d2b5d6cc85d
Wojciech Ziółek <wojciech.ziolek@cern.ch>
2016-03-02 18:04:31 +0100

    global: addition of admin interface
    * Adds basic admin interface. (closes #11) (closes #22)
    
    Reviewed-by: Jiri Kuncar <jiri.kuncar@cern.ch>
    Signed-off-by: Wojciech Ziółek <wojciech.ziolek@cern.ch>

---
+++ invenio_oaiserver/admin.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2015, 2016 CERN.
+#
+# Invenio is free software; you can redistribute it
+# and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# Invenio is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Invenio; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111-1307, USA.
+#
+# In applying this license, CERN does not
+# waive the privileges and immunities granted to it by virtue of its status
+# as an Intergovernmental Organization or submit itself to any jurisdiction.
+
+"""Admin model views for OAI sets."""
+
+from flask_admin.contrib.sqla import ModelView
+
+from .models import OAISet
+
+
+def _(x):
+    """Identity."""
+    return x
+
+
+class OAISetModelView(ModelView):
+    """OAISets model view."""
+
+    can_create = True
+    can_edit = True
+    can_delete = False
+    can_view_details = True
+    column_list = ('id', 'spec', 'updated', 'created',)
+    column_details_list = ('id', 'spec', 'name', 'description',
+                           'search_pattern', 'updated', 'created')
+    column_filters = ('created', 'updated')
+    column_default_sort = ('updated', True)
+    page_size = 25
+
+
+set_adminview = dict(
+    modelview=OAISetModelView,
+    model=OAISet,
+    category=_('OAI Sets'),
+)
