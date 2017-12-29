318f9b908b9227aab7e265278bb9396f0c8b07d1
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2017-12-29 17:35:59 +0100

    tests: pytest 3.3.0 with python 2 fix

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ tests_require = [
     'pytest-cache>=1.0',
     'pytest-cov>=1.8.0',
     'pytest-pep8>=1.0.6',
-    'pytest>=2.8.0',
+    'pytest>=2.8.0,!=3.3.0',
 ]
 
 invenio_search_version = '1.0.0b4'
