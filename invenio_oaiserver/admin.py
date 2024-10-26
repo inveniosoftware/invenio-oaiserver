fff7f684da2f830e7270f29ac54ba987589863d2
Sam Arbid <samaa2@kth.se>
2024-10-26 02:18:08 +0200

    i18n: mark strings for translation

--- invenio_oaiserver/admin.py
+++ invenio_oaiserver/admin.py
@@ -2,6 +2,7 @@
 #
 # This file is part of Invenio.
 # Copyright (C) 2015-2018 CERN.
+# Copyright (C) 2024 KTH Royal Institute of Technology.
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -9,13 +10,9 @@
 """Admin model views for OAI sets."""
 
 from flask_admin.contrib.sqla import ModelView
+from invenio_i18n import gettext as _
 
 from .models import OAISet
-
-
-def _(x):
-    """Identity."""
-    return x
 
 
 class OAISetModelView(ModelView):
