ec51ae6f17407ef6f2b43e57de0e58b1fe5cd086
Florian Gantner <flo.gantner@gmx.de>
2025-05-11 10:34:54 +0200

    fix: serialization of from parameter
    fixes serialization of from parameter

--- invenio_oaiserver/verbs.py
+++ invenio_oaiserver/verbs.py
@@ -122,8 +122,9 @@ class Verbs(object):
 
         from_ = DateTime(
             format="permissive",
-            metadata={"load_from": "from", "data_key": "from", "dump_to": "from"},
+            metadata={"data_key": "from"},
             data_key="from",
+            attribute="from",
         )
         until = DateTime(format="permissive")
         set = fields.Str()
