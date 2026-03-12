42e55c1155a8202c8935d547ed32aa455020a954
Christoph Ladurner <christoph.ladurner@tugraz.at>
2026-03-12 13:57:40 +0100

    release: v4.0.1

--- invenio_oaiserver/__init__.py
+++ invenio_oaiserver/__init__.py
@@ -187,6 +187,6 @@ repository
 from .ext import InvenioOAIServer
 from .proxies import current_oaiserver
 
-__version__ = "4.0.0"
+__version__ = "4.0.1"
 
 __all__ = ("__version__", "InvenioOAIServer", "current_oaiserver")
