2e25e6029fab46415ff55a5d6a9842cbf3b3a794
Mirek Simek <miroslav.simek@cesnet.cz>
2026-05-15 15:21:04 +0200

    feat(tests): added test for alembic migration

---
+++ tests/test_alembic.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2026 CESNET z.s.p.o.
+#
+# Invenio-oaiserver is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+"""Test invenio-oaiserver alembic."""
+
+import pytest
+from invenio_db import db
+from invenio_db.utils import alembic_test_context, drop_alembic_version_table
+
+
+def test_alembic(app):
+    """Test alembic recipes."""
+    ext = app.extensions["invenio-db"]
+
+    if db.engine.name == "sqlite":
+        raise pytest.skip("Upgrades are not supported on SQLite.")
+
+    app.config["ALEMBIC_CONTEXT"] = alembic_test_context()
+
+    # Check that this package's SQLAlchemy models have been properly registered
+    tables = [x for x in db.metadata.tables]
+    assert "oaiserver_set" in tables
+
+    # Check that Alembic agrees that there's no further tables to create.
+    assert len(ext.alembic.compare_metadata()) == 0
+
+    # Drop everything and recreate tables all with Alembic
+    db.drop_all()
+    drop_alembic_version_table()
+    ext.alembic.upgrade()
+    assert len(ext.alembic.compare_metadata()) == 0
+
+    drop_alembic_version_table()
