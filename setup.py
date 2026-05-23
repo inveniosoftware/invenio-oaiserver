8d090875150b50137216c771c0d9a61f7ba72345
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-05-23 21:20:06 +0200

    chore(licenses): update license headers to use SPDX

--- setup.py
+++ setup.py
@@ -1,12 +1,7 @@
-# -*- coding: utf-8 -*-
-#
-# This file is part of Invenio.
-# Copyright (C) 2015-2022 CERN.
-# Copyright (C)      2021 Graz University of Technology.
-# Copyright (C)      2021 TU Wien.
-#
-# Invenio is free software; you can redistribute it and/or modify it
-# under the terms of the MIT License; see LICENSE file for more details.
+# SPDX-FileCopyrightText: 2015-2022 CERN.
+# SPDX-FileCopyrightText: 2021 Graz University of Technology.
+# SPDX-FileCopyrightText: 2021 TU Wien.
+# SPDX-License-Identifier: MIT
 
 """Invenio module that implements OAI-PMH server."""
 
