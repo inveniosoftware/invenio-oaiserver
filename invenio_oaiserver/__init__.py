3c82e5e29a269ca201bb73cc3c3e1e1c49adcb17
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-07-21 16:46:00 +0200

    release: v5.0.1

--- invenio_oaiserver/__init__.py
+++ invenio_oaiserver/__init__.py
@@ -182,6 +182,6 @@ repository
 from .ext import InvenioOAIServer
 from .proxies import current_oaiserver
 
-__version__ = "5.0.0"
+__version__ = "5.0.1"
 
 __all__ = ("__version__", "InvenioOAIServer", "current_oaiserver")
